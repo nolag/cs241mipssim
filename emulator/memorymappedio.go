589341745760805d5e27cab4299efbc1110f10c8
Ryan Tinianov <tinianov@live.com>
2019-01-21 12:57:08 -0800

    Make memory mapped io work as expected

--- emulator/memorymappedio.go
+++ emulator/memorymappedio.go
@@ -4,20 +4,11 @@ import (
 	"github.com/nolag/gocpu/memory"
 )
 
-type boundaryType int
-
-const (
-	crossesBoundary      boundaryType = iota
-	outsideBoundry                    = iota
-	laterBytesInBoundary              = iota
-	firstIsBoundry                    = iota
-)
-
 // StdinRead is the word that is mapped to reading stdio
-const StdinRead = 0xffff0004
+const StdinRead = 0xffff0007
 
 // StdoutWrite is the word mapped to writing to stdout
-const StdoutWrite = 0xffff000c
+const StdoutWrite = 0xffff000F
 
 // Readable wraps reading from a file.
 type Readable interface {
@@ -40,12 +31,10 @@ type MemoryMappedIO struct {
 // return next byte from standard input, other bytes in that word will return 0.
 func (mem *MemoryMappedIO) ReadOneByte(index uint64) (byte, error) {
 	if index == StdinRead {
-		val := make([]byte, 1)
-		_, err := mem.Stdin.Read(val)
-		return val[0], err
-	}
-
-	if index > StdinRead && index < StdinRead+4 {
+		readVal := make([]byte, 1)
+		_, err := mem.Stdin.Read(readVal)
+		return readVal[0], err
+	} else if index < StdinRead && index > StdinRead-4 {
 		return 0, nil
 	}
 
@@ -57,60 +46,30 @@ func (mem *MemoryMappedIO) ReadOneByte(index uint64) (byte, error) {
 // backed, when true means that changes made to data will impact the memory stored
 // err is any error that occured
 // Reads from the word StdinRead will return the next byte from standard output, padded to a word.
-// Crossing the StdinRead word boundary will cause an error to be returned
 func (mem *MemoryMappedIO) ReadRaw(startIndex uint64, numBytes uint64) (data []byte, backed bool, err error) {
-	boundaryCheck := bounderyCheck(startIndex, numBytes, StdinRead)
-	switch boundaryCheck {
-	case crossesBoundary:
+	if !validMemoryLoc(startIndex, numBytes) {
 		return nil, false, &memory.AccessViolationError{Location: startIndex, NumBytes: numBytes, WasRead: true}
-	case firstIsBoundry:
-		read := make([]byte, 1)
-		_, err := mem.Stdin.Read(read)
-		result := make([]byte, numBytes)
-		copy(result, read)
-		return result, false, err
-	case laterBytesInBoundary:
-		return make([]byte, numBytes), false, nil
 	}
 
-	val, backed, err := mem.BackingMemory.ReadRaw(startIndex, numBytes)
-
-	if err == nil {
-		boundaryCheck = bounderyCheck(startIndex, numBytes, StdoutWrite)
-		switch boundaryCheck {
-		case crossesBoundary:
-			fallthrough
-		case firstIsBoundry:
-			fallthrough
-		case laterBytesInBoundary:
-			backed = false
-			val2 := make([]byte, numBytes)
-			copy(val2, val)
-			val = val2
-		}
-	}
-
-	return val, backed, err
-}
-
-func bounderyCheck(startIndex uint64, numBytes uint64, boundaryStart uint64) boundaryType {
-	boundaryEnd := boundaryStart + 3
 	endIndex := startIndex + numBytes - 1
 
-	if (startIndex < boundaryStart && endIndex >= boundaryStart) ||
-		((startIndex >= boundaryStart && startIndex <= boundaryEnd) && endIndex > boundaryEnd) {
-		return crossesBoundary
+	if endIndex == StdinRead {
+		readVal := make([]byte, 1)
+		_, err := mem.Stdin.Read(readVal)
+		retVal := make([]byte, numBytes)
+		retVal[0] = readVal[0]
+		return retVal, false, err
 	}
 
-	if startIndex == boundaryStart {
-		return firstIsBoundry
+	data, backed, err = mem.BackingMemory.ReadRaw(startIndex, numBytes)
+	if endIndex == StdoutWrite {
+		backed = false
+		temp := data
+		data = make([]byte, numBytes)
+		copy(data, temp)
 	}
 
-	if startIndex > boundaryStart && startIndex <= boundaryEnd {
-		return laterBytesInBoundary
-	}
-
-	return outsideBoundry
+	return data, backed, err
 }
 
 // Size in bytes this memory can represent
@@ -120,10 +79,34 @@ func (mem *MemoryMappedIO) Size() uint64 {
 
 // WriteOneByte reads a byte at memory location index
 func (mem *MemoryMappedIO) WriteOneByte(val byte, index uint64) error {
+	if index == StdoutWrite {
+		output := make([]byte, 1)
+		output[0] = val
+		_, err := mem.Stdout.Write(output)
+		return err
+	} else if index < StdoutWrite && index > StdoutWrite-4 {
+		return nil
+	}
 	return mem.BackingMemory.WriteOneByte(val, index)
 }
 
 // WriteRaw writes data back to memory
 func (mem *MemoryMappedIO) WriteRaw(data []byte, startIndex uint64) error {
+	numBytes := uint64(len(data))
+	if !validMemoryLoc(startIndex, uint64(numBytes)) {
+		return &memory.AccessViolationError{Location: startIndex, NumBytes: numBytes, WasRead: false}
+	}
+
+	if startIndex+uint64(len(data))-1 == StdoutWrite {
+		output := make([]byte, 1)
+		output[0] = data[numBytes-1]
+		_, err := mem.Stdout.Write(output)
+		return err
+	}
+
 	return mem.BackingMemory.WriteRaw(data, startIndex)
+}
+
+func validMemoryLoc(startIndex uint64, numBytes uint64) bool {
+	return numBytes == 1 || numBytes == 2 && startIndex&1 == 0 || numBytes == 4 && startIndex&3 == 0
 }
--- emulator/memorymappedio_test.go
+++ emulator/memorymappedio_test.go
@@ -13,17 +13,31 @@ func TestRunReadWriteTest(t *testing.T) {
 	testhelper.RunReadWriteTest(t, true, createMappedForTest)
 }
 
-func TestNewMeorySliceCreatesMemoryWithCorrectSize(t *testing.T) {
+func TestSize(t *testing.T) {
 	testhelper.RunSizeTest(t, createMappedForTest)
 }
 
-func TestSliceBoundsChceking(t *testing.T) {
+func TestBoundsChceking(t *testing.T) {
 	testhelper.RunBoundsTests(t, createMappedForTest)
+}
+
+func TestAllignmentIsPowersOfTwoUntilFourBytes(t *testing.T) {
+	testhelper.RunPowTwoAllignmentFailures(t, 2, createMappedForTest)
+}
+
+func TestReadWriteCannotBeEightOrMoreBytes(t *testing.T) {
+	testhelper.RunDisallowedSize(t, 8, createMappedForTest)
+	testhelper.RunDisallowedSize(t, 16, createMappedForTest)
+}
+
+func TestReadWriteMustMustBePowerOfTwo(t *testing.T) {
+	anyNonPowerOfTwoLessThanFour := uint64(3)
+	testhelper.RunDisallowedSize(t, anyNonPowerOfTwoLessThanFour, createMappedForTest)
 }
 
 func TestReadOneByteReadsFromStdin(t *testing.T) {
 	anyVal := byte(10)
-	reader := fakeReadable{NextVal: anyVal}
+	reader := fakeReadable{NextVal: anyVal, Tester: t}
 	mem := MemoryMappedIO{Stdin: reader}
 
 	actual, err := mem.ReadOneByte(StdinRead)
@@ -34,7 +48,7 @@ func TestReadOneByteReadsFromStdin(t *testing.T) {
 
 func TestReadOneByteRetunsErrorFromStdin(t *testing.T) {
 	anyErr := errors.New("Test")
-	reader := fakeReadable{Err: anyErr}
+	reader := fakeReadable{Err: anyErr, Tester: t}
 	mem := MemoryMappedIO{Stdin: reader}
 
 	_, err := mem.ReadOneByte(StdinRead)
@@ -46,7 +60,7 @@ func TestReadOneByteReturnsZeroForRemainingWordPartForStdin(t *testing.T) {
 	mem := MemoryMappedIO{}
 
 	for i := 1; i < 4; i++ {
-		actual, err := mem.ReadOneByte(StdinRead + uint64(i))
+		actual, err := mem.ReadOneByte(StdinRead - uint64(i))
 		assert.NoError(t, err, "Memory must not return an error when reading off bytes in stdin word")
 		assert.Equal(
 			t,
@@ -56,104 +70,130 @@ func TestReadOneByteReturnsZeroForRemainingWordPartForStdin(t *testing.T) {
 	}
 }
 
-func TestReadOneByteWorksForStdout(t *testing.T) {
+func TestReadRawReadsFromStdin(t *testing.T) {
+	anyVal := byte(10)
+	reader := fakeReadable{NextVal: anyVal, Tester: t}
+	mem := MemoryMappedIO{Stdin: reader}
+
+	for i := uint64(1); i <= 4; i <<= 1 {
+		actual, backed, err := mem.ReadRaw(StdinRead-i+1, i)
+		expected := make([]byte, i)
+		expected[i-1] = anyVal
+		assert.ElementsMatch(t, expected, actual, "Read raw must read from stdin with %v bytes", i)
+		assert.NoError(t, err, "Memory must not return an error when stdin has no error with %v bytes", i)
+		assert.False(t, backed, "Memory from IO is not backed with %v bytes", i)
+	}
+}
+
+func TestReadRawRetunsErrorFromStdin(t *testing.T) {
+	anyErr := errors.New("Test")
+	reader := fakeReadable{Err: anyErr, Tester: t}
+	mem := MemoryMappedIO{Stdin: reader}
+
+	_, _, err := mem.ReadRaw(StdinRead-3, 4)
+
+	assert.Equal(t, anyErr, err, "Error from reading stdin must be propogated")
+}
+
+func TestReadRawReturnsFalseForBackingReadFromStdout(t *testing.T) {
 	anyVal := byte(10)
 	slice := memory.NewSlice(StdoutWrite + 1)
 	slice.WriteOneByte(anyVal, StdoutWrite)
 	mem := MemoryMappedIO{BackingMemory: slice}
 
-	actual, err := mem.ReadOneByte(StdoutWrite)
+	actual, backed, err := mem.ReadRaw(StdoutWrite-3, 4)
 
-	assert.Equal(t, anyVal, actual, "Wrong value returned")
-	assert.NoError(t, err, "Memory must not return an error when reading stdin")
-}
-
-func TestReadRawReadsFromStdin(t *testing.T) {
-	anyVal := byte(10)
-	reader := fakeReadable{NextVal: anyVal}
-	mem := MemoryMappedIO{Stdin: reader}
-
-	actual, backed, err := mem.ReadRaw(StdinRead, 4)
-
-	assert.ElementsMatch(t, []byte{anyVal, 0, 0, 0}, actual, "Read raw must read from stdin")
-	assert.NoError(t, err, "Memory must not return an error when stdin has no error")
+	assert.ElementsMatch(t, []byte{0, 0, 0, anyVal}, actual, "Read must read from memory")
+	assert.NoError(t, err, "Memory must not return an error when reading from stdin boundary")
 	assert.False(t, backed, "Memory from IO is not backed")
+	prior := actual[3]
+	actual[3] = 100
+	afterWrite, _ := mem.ReadOneByte(StdoutWrite)
+	assert.Equal(t, prior, afterWrite, "Non backing memory must not change the value when written to")
+}
+func TestWriteOneByteWritesToStdout(t *testing.T) {
+	anyVal := byte(10)
+	writer := fakeWriteable{Tester: t}
+	mem := MemoryMappedIO{Stdout: &writer}
+
+	err := mem.WriteOneByte(anyVal, StdoutWrite)
+
+	assert.NoError(t, err, "Memory must not return an error when stdout has no error")
+	assert.Equal(t, anyVal, writer.WrittenVal, "Wrong value written to stdout")
 }
 
-func TestReadRawRetunsErrorFromStdin(t *testing.T) {
+func TestWriteOneByteRetunsErrorFromStdout(t *testing.T) {
+	anyVal := byte(10)
 	anyErr := errors.New("Test")
-	reader := fakeReadable{Err: anyErr}
-	mem := MemoryMappedIO{Stdin: reader}
+	writer := fakeWriteable{Err: anyErr, Tester: t}
+	mem := MemoryMappedIO{Stdout: &writer}
 
-	_, _, err := mem.ReadRaw(StdinRead, 4)
+	err := mem.WriteOneByte(anyVal, StdoutWrite)
 
-	assert.Equal(t, anyErr, err, "Error from reading stdin must be propogated")
+	assert.Equal(t, anyErr, err, "Error from reading stdout must be propogated")
 }
 
-func TestReadRawReturnsZeroForRemainingWordPartForStdin(t *testing.T) {
+func TestWriteOneByteReturnsNoErrorForRemainingWordPartForStdin(t *testing.T) {
+	anyVal := byte(10)
 	mem := MemoryMappedIO{}
 
 	for i := 1; i < 4; i++ {
-		actual, _, err := mem.ReadRaw(StdinRead+uint64(i), 4-uint64(i))
-		assert.NoError(t, err, "Memory must not return an error when reading off bytes in stdin word")
-		assert.Equal(
-			t,
-			make([]byte, 4-uint64(i)),
-			actual,
-			"To match reading from the word, 0 must be returned when reading off bytes in stdin word")
+		err := mem.WriteOneByte(anyVal, StdoutWrite-uint64(i))
+		assert.NoError(t, err, "Memory must not return an error when writting off bytes in stdout word")
+	}
+}
+func TestWriteRawWritesToStdout(t *testing.T) {
+	anyVal := byte(10)
+	writer := fakeWriteable{Tester: t}
+	mem := MemoryMappedIO{Stdout: &writer}
+
+	for i := uint64(1); i <= 4; i <<= 1 {
+		writer.WrittenVal = 0
+		anyVals := make([]byte, i)
+		anyVals[i-1] = anyVal
+
+		err := mem.WriteRaw(anyVals, StdoutWrite-i+1)
+
+		assert.NoError(t, err, "Memory must not return an error when stdout has no error for %v bytes", i)
+		assert.Equal(t, anyVal, writer.WrittenVal, "wrong value written for %v bytes", i)
 	}
 }
 
-func TestReadRawReturnsAccessErrorIfCrossingBoundaryForMemoryRead(t *testing.T) {
-	mem := MemoryMappedIO{}
-	readLoc := StdinRead + uint64(1)
-	readLoc2 := uint64(StdinRead)
-	readLoc3 := StdinRead - uint64(1)
-	_, _, err := mem.ReadRaw(readLoc, 4)
-	_, _, err2 := mem.ReadRaw(readLoc2, 5)
-	_, _, err3 := mem.ReadRaw(readLoc3, 2)
+func TestWriteRawRetunsErrorFromStdout(t *testing.T) {
+	anyVals := []byte{10, 2, 21, 3}
+	anyErr := errors.New("Test")
+	writer := fakeWriteable{Err: anyErr, Tester: t}
+	mem := MemoryMappedIO{Stdout: &writer}
 
-	expected := memory.AccessViolationError{Location: readLoc, NumBytes: 4, WasRead: true}
-	expected2 := memory.AccessViolationError{Location: readLoc2, NumBytes: 5, WasRead: true}
-	expected3 := memory.AccessViolationError{Location: readLoc3, NumBytes: 2, WasRead: true}
-	assert.Equal(t, &expected, err)
-	assert.Equal(t, &expected2, err2)
-	assert.Equal(t, &expected3, err3)
+	err := mem.WriteRaw(anyVals, StdoutWrite-3)
+
+	assert.Equal(t, anyErr, err, "Error from reading stdout must be propogated")
 }
-
-func TestReadRawReturnsFalseForBackingReadFromStdout(t *testing.T) {
-	anyVal := byte(10)
-	slice := memory.NewSlice(StdoutWrite + 3)
-	slice.WriteOneByte(anyVal, StdoutWrite)
-	mem := MemoryMappedIO{BackingMemory: slice}
-
-	actual, backed, err := mem.ReadRaw(StdoutWrite-2, 5)
-
-	assert.ElementsMatch(t, []byte{0, 0, anyVal, 0, 0}, actual, "Read must read from memory")
-	assert.NoError(t, err, "Memory must not return an error when crossing stdin boundary")
-	assert.False(t, backed, "Memory from IO is not backed")
-	prior := actual[0]
-	actual[0] = 100
-	afterWrite, _ := mem.ReadOneByte(StdoutWrite - 2)
-	assert.Equal(t, prior, afterWrite, "Non backing memory must not change the value when written to")
-}
-
-// TODO maybe I want to force word alignment on multi-byte reads, it will simply the logic a lot.
-// maybe I'll also enforce reads on 4 bytes, since the machine doesn't allow otherwise.
-
-// TODO test single byte writes
-// TODO test raw writes from location and crossing boundary
 
 func createMappedForTest(size uint64) memory.Memory {
 	return &MemoryMappedIO{BackingMemory: memory.NewSlice(size)}
 }
 
 type fakeReadable struct {
-	NextVal byte
 	Err     error
+	NextVal byte
+	Tester  *testing.T
 }
 
 func (readable fakeReadable) Read(b []byte) (n int, err error) {
+	assert.Equal(readable.Tester, 1, len(b), "This memory must only read one byte")
 	b[0] = readable.NextVal
 	return 1, readable.Err
+}
+
+type fakeWriteable struct {
+	Err        error
+	Tester     *testing.T
+	WrittenVal byte
+}
+
+func (writeable *fakeWriteable) Write(b []byte) (n int, err error) {
+	assert.Equal(writeable.Tester, 1, len(b), "This memory must only write one byte")
+	writeable.WrittenVal = b[0]
+	return len(b), writeable.Err
 }
